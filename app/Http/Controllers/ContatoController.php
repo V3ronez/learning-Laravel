7cadbab4b8c2267a11b21910900f75e4bc54e331
v3ronez <v3ronez.dev@gmail.com>
2022-06-19 19:48:42 -0300

    trabalhando com forms

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -3,12 +3,35 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\SiteContato;
 
 class ContatoController extends Controller
 {
-    public function contato()
+    public function contato(Request $request)
     {
-        var_dump($_POST);
-        return view('site.contato',['titulo'=> 'titulo Controller']);
+        // echo('<pre>');
+        // print_r($request->all()); //acessar as info encaminhas pelos forms (O ARRAY DE PARAM)
+        // echo('</pre>');
+        // echo('</br');
+        // $name = $request->input('name');
+        // echo($name); //acessar o valor de um input especifico
+
+
+        // 1-METODO DE SALVAR NO DB; $contato->(nome da coluna no db);
+        $contato = new SiteContato();
+
+        $contato->name = $request->input('name');
+        $contato->phone = $request->input('telefone');
+        $contato->email = $request->input('email');
+        $contato->contact_reason = $request->input('motivo_contato');
+        $contato->messenger = $request->input('textarea');
+        $contato->save();
+
+        //2-METODO DE PERSISTIR NO DB.
+        // $contato = new SiteContato();
+        // $contato->fill($request->all());
+        // $contato->save();
+
+        return view('site.contato', ['titulo' => 'titulo Controller']);
     }
 }
--- app/Models/SiteContato.php
+++ app/Models/SiteContato.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class SiteContato extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name', 'phone', 'email', 'contact_reason', 'messenger'];
 }
